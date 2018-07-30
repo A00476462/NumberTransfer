3e426bc77f17896598a9e54170a939a79d783ce0
guoli_dlmu <guoli_dlmu@163.com>
2018-07-30 08:25:21 -0400

    number transfer

---
+++ NumerTransfer/src/numtrasfer/Main.java
@@ -0,0 +1,15 @@
+package numtrasfer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Transfer tran = new Transfer();
+		tran.toBin(7);
+		tran.toOctal(7);
+		tran.toHex(7);
+	}
+
+	
+
+}
